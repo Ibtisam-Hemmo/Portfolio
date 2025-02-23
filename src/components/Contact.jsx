8d424245a3be908793d35a7fb8b99573d1d30a9a
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-23 12:02:58 +0200

    add form validation and fix style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,36 +2,59 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div name='contact' className="w-full h-fit bg-gradient-to-b from-black to-gray-800 text-white" >
+    <div
+      name="contact"
+      className="w-full h-fit bg-gradient-to-b from-black to-gray-800 text-white"
+    >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8 sm:mt-20">
-          <p className="text-4xl font-bold inline border-b-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text">GET IN TOUCH</p>
-          <p className="py-6">Please fill in the form below, and I'll get back to you right away if you have any constructive comments, projects, or even just want to say hi.</p>
+          <p className="text-4xl font-bold inline border-b-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text">
+            GET IN TOUCH
+          </p>
+          <p className="py-6">
+            Please fill in the form below, and I'll get back to you right away
+            if you have any constructive comments, projects, or even just want
+            to say hi.
+          </p>
         </div>
         <div className="flex justify-center items-center">
-          <form action="https://getform.io/f/107ac08c-58b3-4eaf-b3b0-dc5117aa016e"  method="POST" className="flex flex-col w-full md:w-1/2">
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter Your name"
-              className=" p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
+          <form
+            action="https://getform.io/f/107ac08c-58b3-4eaf-b3b0-dc5117aa016e"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2"
+          >
             <input
               type="text"
               name="name"
+              placeholder="Enter Your name"
+              className=" p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              onInvalid={(e) => e.target.setCustomValidity("Please enter your name.")}
+              onInput={(e) => e.target.setCustomValidity("")}
+              required
+            />
+            <input
+              type="email"
+              name="email"
               placeholder="Enter Your Email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+              onInvalid={(e) => e.target.setCustomValidity("Please enter a valid email address.")}
+              onInput={(e) => e.target.setCustomValidity("")}
+              required
             />
             <textarea
               name="message"
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              required
             ></textarea>
-            <button className="bg-gradient-to-b  from-cyan-300 to-blue-500 px-6 py-3 my-8  max-auto flex items-center rounded-md hover:scale-110 duration-300 text-white">Lets' talk</button>
+            <button className="bg-gradient-to-b  from-cyan-300 to-blue-500 px-6 py-3 my-8  max-auto flex items-center rounded-md hover:scale-105 hover:font-bold duration-300 text-white">
+              Lets' talk
+            </button>
           </form>
         </div>
-        <p className="text-center">By Ibtisam Hemmo 2023</p>
+        <p className="text-center">By Ibtisam Hemmo 2025</p>
       </div>
     </div>
   );
